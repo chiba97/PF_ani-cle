a5890b8e96bfaa3db7653246df8012357d13ad08
chiba97 <ktfkyrya@gmail.com>
2021-10-31 08:52:30 +0000

    Add デバイス

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,53 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # デバイスUser側
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "user/registrations",
+    sessions: "user/sessions"
+  }
+
+  # デバイスAdmin側
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+  # Admin側ルーティング
+  namespace :admin do
+    resources :users, except: [:new, :create, :destroy]
+    get "searches" => "searches#search"
+  end
+
+  # User側ルーティング
+  scope module: :user do
+  root to: "homes#top"
+  get "/about" => "homes#about"
+  get "searches" => "searches#search"
+  resources :rooms, only: [:show, :create]
+  resources :messages, only: [:create]
+
+  resources :posts do
+    resources :comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+
+  resources :users, only: [:show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    member do
+      get  "follows"
+      get "followers"
+      get "favorites"
+    end
+    collection do
+      get "unsubscribe"
+      patch "withdraw"
+    end
+  end
+
+  resources :notifications, only: [:index] do
+    collection do
+      delete "destroy_all"
+    end
+  end
+  end
+
 end
