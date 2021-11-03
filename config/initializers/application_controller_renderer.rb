d180467700d152efaf5cffd3682c31d6f4585104
chiba97 <ktfkyrya@gmail.com>
2021-11-03 06:37:09 +0000

    投稿機能途中

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '8f58ac49b73e90e1fefe05de88b62a4569bd5d22de01dfab0a6a94d0962b14b426fa9c8ba9d36d45a9cde3560d28dc528d113fb100c73c134b5e37cd8ece46a8'
