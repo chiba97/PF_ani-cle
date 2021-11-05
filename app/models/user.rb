0bde9e1c66e881908f727f69c51aea2e5aad9787
chiba97 <ktfkyrya@gmail.com>
2021-11-05 07:47:11 +0000

    [Add]コメントいいねのモデルとコントローラー追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   
   validates :name, presence: true, uniqueness: true, length: {minimum: 2, maximum: 20}
   validates :pet, length: {maximum: 20}
