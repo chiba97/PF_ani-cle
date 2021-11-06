3f374d2e59a867c9f721b7beb9ec47ed658c9962
chiba97 <ktfkyrya@gmail.com>
2021-11-06 03:26:06 +0000

    [Add]コメント機能非同期化

--- app/controllers/user/comments_controller.rb
+++ app/controllers/user/comments_controller.rb
@@ -2,16 +2,15 @@ class User::CommentsController < ApplicationController
   before_action :authenticate_user!
   
   def create
-    post = Post.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     comment = current_user.comments.new(comment_params)
-    comment.post_id = post.id
+    comment.post_id = @post.id
     comment.save
-    redirect_to post_path(post)
   end
   
   def destroy
+    @post = Post.find(params[:post_id])
     Comment.find_by(id: params[:id]).destroy
-    redirect_to post_path(params[:post_id])
   end
   
   private
