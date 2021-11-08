e28286c4b8fda8da2f76c79351d431d1dc5078f5
chiba97 <ktfkyrya@gmail.com>
2021-11-08 08:45:30 +0000

    [Add]チャット機能非同期化

--- app/controllers/user/messages_controller.rb
+++ app/controllers/user/messages_controller.rb
@@ -1,15 +1,16 @@
 class User::MessagesController < ApplicationController
-  
+
   def create
     if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
       @message = Message.create(message_params)
-      redirect_to room_path(@message.room_id)
     end
+    @room = Room.find(params[:message][:room_id])
+    @messages = @room.messages
   end
-  
+
   private
   def message_params
     params.require(:message).permit(:user_id, :room_id, :content).merge(user_id: current_user.id)
   end
-  
+
 end
