629fb0fbef6c9faa8538d801bd1525f4c78f62d1
chiba97 <ktfkyrya@gmail.com>
2021-11-12 03:26:28 +0000

    [start]お問い合わせ機能

--- app/controllers/user/messages_controller.rb
+++ app/controllers/user/messages_controller.rb
@@ -1,4 +1,5 @@
 class User::MessagesController < ApplicationController
+  
   def create
     if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
       @message = Message.create(message_params)
