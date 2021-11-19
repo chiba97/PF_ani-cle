591a7980f6b1c99d2d05a9457c2ed47fe4c8b3ad
chiba97 <ktfkyrya@gmail.com>
2021-11-19 12:56:31 +0000

    [Add]entry単体テスト

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,3 +1,25 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+
+RSpec.describe 'Entryモデルのテスト', type: :model do
+  
+  describe "モデルのテスト" do
+    it "有効なEntryの場合は保存されるか" do
+      expect(build(:entry)).to be_valid
+    end
+  end
+  
+  describe 'アソシエーションのテスト' do
+    context 'Userモデルとの関係' do
+      it '1+Nとなっているか' do
+        expect(Entry.reflect_on_association(:user).macro).to eq :belongs_to
+      end
+    end
+    context 'Roomモデルとの関係' do
+      it '1+Nとなっているか' do
+        expect(Entry.reflect_on_association(:room).macro).to eq :belongs_to
+      end
+    end
+  end
+end
