1586d24823a6854eb3dfaea5132daf00f93f13e2
chiba97 <ktfkyrya@gmail.com>
2021-11-19 09:35:04 +0000

    [Add]Contactモデルテスト記述

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ApplicationRecord
   belongs_to :user
 
-  validates :title, presence: true
-  validates :body, presence: true
+  validates :title, presence: true, length: { maximum: 30}
+  validates :body, presence: true, length: { maximum: 200}
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,3 +1,48 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+
+RSpec.describe 'Contactモデルのテスト', type: :model do
+  describe 'バリデーションのテスト' do
+    subject { contact.valid? }
+    let(:user) {create(:user)}
+    let!(:contact) {build(:contact, user_id: user.id)}
+
+    context 'titleカラム' do
+      it '空欄ではないこと' do
+        contact.title = ''
+        is_expected.to eq false
+      end
+      it '３０文字以内であること：３０文字は○' do
+        contact.title = Faker::Lorem.characters(number: 30)
+        is_expected.to eq true
+      end
+      it '３０文字以内であること：３１文字は×' do
+        contact.title = Faker::Lorem.characters(number: 31)
+        is_expected.to eq false
+      end
+    end
+    context 'bodyカラム' do
+      it '空欄ではないこと' do
+        contact.body = ''
+        is_expected.to eq false
+      end
+      it '２００文字以内であること：２００文字は◯' do
+        contact.body = Faker::Lorem.characters(number: 200)
+        is_expected.to eq true
+      end
+      it '２００文字以内であること：２０１文字は×' do
+        contact.body = Faker::Lorem.characters(number: 201)
+        is_expected.to eq false
+      end
+    end
+  end
+
+  describe 'アソシエーションのテスト' do
+    context 'Userモデルとの関係' do
+      it '1対Nであること' do
+        expect(Contact.reflect_on_association(:user).macro).to eq :belongs_to
+      end
+    end
+  end
+end
