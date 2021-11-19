fbb756a29341f0f080aabf41a4d02630aeca67fe
chiba97 <ktfkyrya@gmail.com>
2021-11-19 13:19:38 +0000

    [Add]relationship単体テスト

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,3 +1,19 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+
+RSpec.describe 'Relationshipモデルのテスト', type: :model do
+  
+  describe 'アソシエーションのテスト' do
+    context 'フォロー(フォローする人)モデルとの関係' do
+      it '1+Nとなっているか' do
+        expect(Relationship.reflect_on_association(:following).macro).to eq :belongs_to
+      end
+    end
+    context 'フォロワー（フォローされる人）モデルとの関係' do
+      it '1+Nとなっているか' do
+        expect(Relationship.reflect_on_association(:follower).macro).to eq :belongs_to
+      end
+    end
+  end
+end
